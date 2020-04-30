5800a9c350bcdf45f8c3c380b59c10de2c74e0c4
kaushik <shivamkaushik12007@gmail.com>
2020-04-30 18:01:01 +0530

    day 6 frontend\

--- curiousask/src/components/intro.jsx
+++ curiousask/src/components/intro.jsx
@@ -2,60 +2,103 @@ import React, {Component} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Intro extends Component{
+    constructor(){
+        super();
+        this.state={
+            pnm:false,
+            enm:false,
+            unm:false,
+            lnm:false
+        }
+    }
     render(){
         return(
-            <div>
-                <div className="row" style={{height:'10vh',padding:'2vh'}}>
-                    <div className="col-sm-6" style={{paddingLeft:'2vw'}}><h1>CuriousAsk</h1></div>
-                    <div className="col-sm-6">
-                        <form>
-                            <div className="form-group row" style={{paddingLeft:'10vw'}}>
-                                <div className="col-xs-4">
-                                    <input type="text" placeholder="UserName" className="form-control"></input>
+            <div style={{widht:'vw'}}>
+                <div className="row  align-middle h-50 p-3 bg-danger">
+                    <div className="col-sm-7 display-4 text-light font-weight-bolder">CuriousAsk</div>
+                    <div className="col-sm-5 pt-3">
+                        <form onSubmit={this.login}>
+                            <div className="form-group row">
+                                <div className="col-xs-4 p-1">
+                                    <input type="text" placeholder="UserName" className="form-control" ref="logUser"></input>
                                 </div>
-                                <div className="col-xs-4" style={{paddingLeft:'1vw'}}>
-                                    <input type="password" placeholder="Password" className="form-control"></input>
+                                <div className="col-xs-4 p-1">
+                                    <input type="password" placeholder="Password" className="form-control" ref="logPass"></input>
                                 </div>
-                                <div className="col-xs-4" style={{paddingLeft:'1vw'}}>
+                                <div className="col-xs-4 p-1">
                                     <button type="submit"  className="btn btn-primary" >logIn</button>
                                 </div>   
                             </div>
                         </form>
+                        {this.state.lnm?(<div className="text-light text-center font-weight-bold">UserName or Password is wrong...try again!</div>)
+                        :<p></p>}
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-sm-7">
+                <div className="row bg-light pt-5" style={{height:'80vh'}}>
+                    <div className="col-sm-7 display-2 text-danger font-weight-bolder text-center">
+                        Curious about <br/> something...<br/>Ask it here!
                     </div>
-                    <div className="col-sm-5 form-group" style={{padding:'3vw'}}>
-                        <form>
-                            <div className="row" style={{padding:'1vh'}}>
-                                <div className="col-sm-6">
-                                    <input type="text" placeholder="First Name" className="form-control"></input>
+                    <div className="col-sm-5 form-group px-5">
+                        <form onSubmit={this.signup}>
+                            <div className="row pl-3 pr-3">
+                                <div className="col-sm-6 pr-1">
+                                    <input type="text" placeholder="First Name" className="form-control" ref="fname"></input>
                                 </div>
-                                <div className="col-sm-6">
-                                    <input type="text" placeholder="Last Name" className="form-control"></input>
+                                <div className="col-sm-6 pl-1">
+                                    <input type="text" placeholder="Last Name" className="form-control" ref="lname"></input>
                                 </div>
                             </div>
-                            <div className="col-sm-12" style={{padding:'1vh'}}>
-                                <input type="text" placeholder="UserName" className="form-control"></input>
+                            <div className="col-sm-12 pt-3">
+                                <input type="text" placeholder="UserName" className="form-control" ref="uname"></input>
                             </div>
-                            <div className="col-sm-12" style={{padding:'1vh'}}>
-                                <input type="email" placeholder="Email" className="form-control"></input>
+                            <div className="col-sm-12 pt-3">
+                                <input type="email" placeholder="Email" className="form-control" ref="email"></input>
                             </div>
-                            <div className="col-sm-12" style={{padding:'1vh'}}>
-                                <input type="password" placeholder="Password" className="form-control"></input>
+                            <div className="col-sm-12 pt-3">
+                                <input type="password" placeholder="Password" className="form-control" ref="pass"></input>
                             </div>
-                            <div className="col-sm-12" style={{padding:'1vh'}}>
-                                <input type="password" placeholder="Rewrite Password" className="form-control"></input>
+                            <div className="col-sm-12 pt-3">
+                                <input type="password" placeholder="Rewrite Password" className="form-control" ref="pass2"></input>
                             </div>
-                            <div className="col-sm-12" style={{padding:'1vh',textAlign:'center'}}>
-                                <button type="submit"  className="btn btn-primary" >SignUp</button>
+                            {this.state.pnm?(<div className="pt-3 pl-3 text-danger text-left">Your password does not match!</div>)
+                            :this.state.enm?(<div className="pt-3 pl-3 text-danger text-left">This email is already in use!</div>)
+                            :this.state.unm?(<div className="pt-3 pl-3 text-danger text-left">This username is taken try another!</div>)
+                            :<p></p>}
+                            <div className="col-sm-12 pt-3 text-center">
+                                <button type="submit"  className="btn btn-primary">SignUp</button>
                             </div>
                         </form>
                     </div>
                 </div>   
             </div>
         )
+    }
+    
+    login=(event)=>{
+        event.preventDefault();
+        var user={
+            userName:this.refs.logUser.value,
+            password:this.refs.logPass.value
+        }
+        console.log(user);
+    }
+
+    signup=(event)=>{
+        event.preventDefault();
+        var p1=this.refs.pass.value;
+        var p2=this.refs.pass2.value;
+        if(p1===p2){       
+            var user={
+                username:this.refs.uname.value,
+                password:this.refs.pass.value,
+                firstName:this.refs.fname.value,
+                lastName:this.refs.lname.value,
+                eMail:this.refs.email.value
+            }
+            console.log(user);
+        }else{
+            this.setState({pnm:true})
+        }
     }
 }
 export default Intro;
--- curiousask/src/components/profilePage.jsx
+++ curiousask/src/components/profilePage.jsx
@@ -13,12 +13,50 @@ class ProfilePage extends Component{
     }
     render(){
         return(
-            <div>
-                <button onClick={this.changeOne}>home</button>
-                <button onClick={this.changeTwo}>profile</button>
-                <button onClick={this.changeThree}>information</button>
-                {this.state.check===0?(<Home/>):this.state.check===1?(<MyQuestion/>):(<Profile/>)}
+            <div style={{widht:'vw'}}>
+                <div className="row  align-middle h-50 p-3 bg-danger">
+                    <div className="col-sm-3 display-4 text-light font-weight-bolder">CuriousAsk</div>
+                    <div className="col-sm-3 pt-3">
+                        <form onSubmit={this.login}>
+                            <div className="form-group d-flex flex-row-reverse row">
+                                <div className="col-xs-4 pt-1">
+                                    <button type="submit"  className="btn btn-primary">search</button>
+                                </div>   
+                                <div className="col-xs-8 pt-1">
+                                    <input type="text" placeholder="UserName" className="form-control" ref="logUser"></input>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className="col-sm-6 d-flex flex-row-reverse row pt-3">
+                        <div className="col-xs-3">
+                            <button type="submit"  className="btn text-light btn-lg">logout</button>
+                        </div>
+                        <div className="col-xs-3">
+                            <button type="submit" className="btn text-light btn-lg">Profile</button>
+                        </div>
+                        <div className="col-xs-3">
+                            <button type="submit"  className="btn text-light btn-lg">My Questions</button>
+                        </div>
+                        <div className="col-xs-3">
+                            <button type="submit"  className="btn text-light btn-lg">Home</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="row bg-light pt-5" style={{height:'80vh'}}>
+                    <div className="col-sm-3 display-2 text-danger font-weight-bolder text-center">
+                    </div>
+                    <div className="col-sm-9 form-group px-5">
+                        
+                    </div>
+                </div>   
             </div>
+            // <div>
+            //     <button onClick={this.changeOne}>home</button>
+            //     <button onClick={this.changeTwo}>profile</button>
+            //     <button onClick={this.changeThree}>information</button>
+            //     {this.state.check===0?(<Home/>):this.state.check===1?(<MyQuestion/>):(<Profile/>)}
+            // </div>
         )
     }
     changeOne=()=>{
