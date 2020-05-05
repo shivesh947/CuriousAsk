a9ce1b45eff739086a1efc63115452eec130ea18
kaushik <shivamkaushik12007@gmail.com>
2020-05-05 17:26:09 +0530

    backend day 1

---
+++ curiousaskBackend/src/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose');
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.openUri('',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+var db=mongoose.connection;
+db.on('error',function(){
+    console.log("Error connecting to db")
+})
+
+db.once('open',function(){
+    console.log("Connected to db")
+})
